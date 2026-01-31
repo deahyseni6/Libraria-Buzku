9c3649aa19fe2a88be16aa238045d16a3dcca494
SiboreKlinaku <sk73271@ubt-uni.net>
2026-01-31 21:04:32 +0100

    .

--- Users.php
+++ Users.php
@@ -1,10 +1,9 @@
 <?php
-include 'db_connect.php'; // lidhja me databazën
+include 'db_connect.php'; 
 
 $users = [];
 
 try {
-    // Merr të gjithë përdoruesit nga tabela users
     $stmt = $conn->query("SELECT * FROM users");
     $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
