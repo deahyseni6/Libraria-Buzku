3ec363ee0ca0a24419f6f6f6dbd33138646c3ba2
SiboreKlinaku <sk73271@ubt-uni.net>
2026-02-01 21:50:24 +0100

    .

--- Dashboard.php
+++ Dashboard.php
@@ -13,7 +13,7 @@
         </div>
 
         <ul>
-            <li><a href="hp.php">Home</a></li>
+            <li><a href="index.php">Home</a></li>
             <div class="dropdown">
                 <button class="drop" style="font-family: garamond;"><a href="Books.php" style="text-decoration: none; color: rgb(211, 178, 95);">Books</a></button>
                 <div class="content">
--- db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$serverName = "localhost"; 
-$database = "projekt_web_db";
-$username = "sa"; 
-$password = "passwordi_yt";
-$conn = new PDO("sqlsrv:Server=$serverName;Database=$database", $username, $password);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
